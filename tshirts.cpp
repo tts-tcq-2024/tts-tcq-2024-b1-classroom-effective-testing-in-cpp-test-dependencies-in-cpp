d2ee8adaeaec8e8939e119f399932d07bed30513
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-08-05 15:25:35 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,54 +1,29 @@
 #include <iostream>
-#include <assert.h>
 #include <string>
-#include <sstream>
+#include <assert.h>
 
-std::string printColorPair(int pairNumber, const char* majorColor, const char* minorColor) {
-    std::ostringstream oss;
-    oss << pairNumber << " | " << majorColor << " | " << minorColor;
-    return oss.str();
+std::string size(int cms) {
+    if(cms < 38) {
+        return "S";
+    } else if(cms >= 38 && cms < 42) {
+        return "M";
+    } else if(cms >= 42) {
+        return "L";
+    }
+    return "NA";
+}
+
+void testSize() {
+    assert(size(37) == "S");
+    assert(size(38) == "M");
+    assert(size(40) == "M");
+    assert(size(42) == "L");
+    assert(size(43) == "L");
+    assert(size(0) == "NA");  // Add test for value outside the range
 }
 
 int main() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    std::string expectedOutput[] = {
-        "0 | White | Blue",
-        "1 | White | Orange",
-        "2 | White | Green",
-        "3 | White | Brown",
-        "4 | White | Slate",
-        "5 | Red | Blue",
-        "6 | Red | Orange",
-        "7 | Red | Green",
-        "8 | Red | Brown",
-        "9 | Red | Slate",
-        "10 | Black | Blue",
-        "11 | Black | Orange",
-        "12 | Black | Green",
-        "13 | Black | Brown",
-        "14 | Black | Slate",
-        "15 | Yellow | Blue",
-        "16 | Yellow | Orange",
-        "17 | Yellow | Green",
-        "18 | Yellow | Brown",
-        "19 | Yellow | Slate",
-        "20 | Violet | Blue",
-        "21 | Violet | Orange",
-        "22 | Violet | Green",
-        "23 | Violet | Brown",
-        "24 | Violet | Slate"
-    };
-
-    int index = 0;
-    for(int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++) {
-            std::string output = printColorPair(index, majorColor[i], minorColor[j]);
-            assert(output == expectedOutput[index]); // This should fail due to the misalignment in the current implementation
-            index++;
-        }
-    }
-    
+    testSize();
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
