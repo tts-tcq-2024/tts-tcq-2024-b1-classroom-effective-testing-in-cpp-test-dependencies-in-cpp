e8d527dd9f21c5f6eabc3992f7f2689993254b60
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-08-08 11:56:55 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-
+// bugor error is the value 38 and 42 are not properly categorized in the current implementation.
 char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
@@ -17,6 +17,12 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
+    
+    // Additional tests to ensure failure
+    assert(size(38) == 'S'); // This will fail since 38 is not handled correctly
+    assert(size(42) == 'M'); // This will fail since 42 is not handled correctly
+    assert(size(0) == '\0'); // This will fail since 0 should not be categorized
+    
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
